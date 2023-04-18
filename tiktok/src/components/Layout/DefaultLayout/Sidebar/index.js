b3494454e904d155c8b3141fea94361d1bd3427c
DESKTOP-5QTAHSG\WU_MING <mrdzung92@gmail.com>
2023-04-19 05:50:05 +0800

    Dựng khung Layout mặc định

--- tiktok/src/components/Layout/DefaultLayout/Sidebar/index.js
+++ tiktok/src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,5 +1,12 @@
+
+import styles from './Sidebar.module.scss'
+import classNames from 'classnames/bind'
+const cx = classNames.bind(styles)
+
 function Sidebar() {
-    return <h2>Sidebar</h2>
+    return <aside className={cx('wrapper')}>
+        <h1>sidevar</h1>
+    </aside>
 }
 
 export default Sidebar
